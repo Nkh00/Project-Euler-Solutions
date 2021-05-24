583d5e4f2e1328c56a13f5397040098415aebbd7
blue4400 <74251916+blue4400@users.noreply.github.com>
2021-05-24 14:11:54 -0400

    Create largest_palindrome_product.py

---
+++ largest_palindrome_product.py
@@ -0,0 +1,8 @@
+possible_numbers = []
+
+for x in reversed(range(100,999)):
+    for y in reversed(range(100, 999)):
+        if str(x * y) == str(x * y)[::-1]:
+            possible_numbers.append(x*y)
+
+print(max(possible_numbers))
