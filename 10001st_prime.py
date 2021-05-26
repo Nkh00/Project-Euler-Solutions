9663595e5715aeb8c0e432e46a0faa068225e50d
nkh00 <74251916+blue4400@users.noreply.github.com>
2021-05-26 12:57:05 -0400

    Update 10001st_prime.py

--- 10001st_prime.py
+++ 10001st_prime.py
@@ -1,12 +1,12 @@
 from largest_prime_factor import is_prime
 
-primes_generated = 3
+primes_generated = 1
 test_number = 5
 
 while primes_generated < 10001:
     if is_prime(test_number, 10):
         primes_generated += 1
+        print(10001-primes_generated)
+    if primes_generated == 10001:
         print(test_number)
     test_number += 2
-
-print(test_number)
